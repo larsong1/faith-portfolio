14a1f0843b1a61b89985f85947d6335f11f19616
Gabriel Larson <gabrieljosephlarson@gmail.com>
2025-01-19 17:39:17 -0700

    Update layout and styles in RootLayout and Home components

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,11 @@ import React from 'react';
 import Image from 'next/image';
 
 export default function Home() {
-  const imgs = [
-    { src: 'asajj.jpg', classes: '' },
-    { src: 'drippy-trooper.jpg', classes: '' },
-    { src: 'grievous.jpg', classes: '' },
-    { src: 'random-sith.jpg', classes: '' },
-    { src: 'retro-poster.jpg', classes: 'col-span-2' },
-    { src: 'rey.jpg', classes: '' },
-    { src: 'vader-art.jpg', classes: '' },
-    { src: 'vader-luke.jpeg', classes: '' },
-    { src: 'vader-tropers.jpg', classes: 'col-span-2' },
-    { src: 'windu.jpg', classes: '' },
-  ];
+  const imgs = [{ src: 'asajj.jpg', classes: '' }];
 
   return (
     <main>
-      <div className={'grid grid-cols-3 gap-2'}>
+      <div className="grid grid-cols-12 gap-6">
         {imgs.map((img, index) => (
           <div
             key={index}
