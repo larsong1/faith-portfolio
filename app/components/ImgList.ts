62e30d4537b8174c23da565d37a184c10b0a3bd9
Gabriel Larson <gabrieljosephlarson@gmail.com>
2025-02-06 23:29:29 -0700

    Refactor Header component layout, update ImgList class names for responsiveness, and adjust grid structure in RootLayout and Home components

--- app/components/ImgList.ts
+++ app/components/ImgList.ts
@@ -1,8 +1,11 @@
 export const imgList = [
-  { src: 'mag cover mockup.png', classes: 'col-span-7 row-span-8' },
-  { src: 'vina soda.png', classes: 'col-span-5 row-span-6' },
-  { src: 'pink floyd.png', classes: 'col-span-5 row-span-8' },
-  { src: 'book covers.png', classes: 'col-span-7 row-span-9' },
-  { src: '5th ave.png', classes: 'col-span-5 row-span-10' },
-  { src: 'cards 3.png', classes: 'col-span-7 row-span-8' },
+  { src: 'mag cover mockup.png', classes: 'lg:col-span-7 lg:row-span-6' },
+  { src: 'vina soda.png', classes: 'lg:col-span-5 lg:row-span-4' },
+  { src: 'pink floyd.png', classes: 'lg:col-span-5 lg:row-span-6' },
+  { src: 'book covers.png', classes: 'lg:col-span-7 lg:row-span-7' },
+  {
+    src: '5th ave.png',
+    classes: 'lg:col-span-5 lg:row-span-7 max-[1500px]:row-span-8',
+  },
+  { src: 'cards 3.png', classes: 'lg:col-span-7 lg:row-span-6' },
 ];
